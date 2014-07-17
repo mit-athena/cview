0906ff3d7642a21d8fd08ed9356dbfbd1062acd6
Jesse Selover <jselover@mit.edu>
2014-07-17 00:13:00 -0400

    Works! Added HXT.

--- src/Main.hs
+++ src/Main.hs
@@ -1,30 +1,113 @@
 {-# LANGUAGE OverloadedStrings #-}
-import           Control.Monad                  (when)
-import           Data.List                      (intercalate)
+import           Control.Applicative            ((<*>))
+import           Control.Monad                  (void, when)
+import           Data.List                      (foldl1', intercalate)
 import           Data.String                    (fromString)
+import           System.Process                 (readProcess)
 import           Text.Blaze.Svg                 (l, m, z)
 import           Text.Blaze.Svg.Internal        (appendToPath)
 import           Text.Blaze.Svg.Renderer.String (renderSvg)
 import           Text.Blaze.Svg11               ((!))
 import qualified Text.Blaze.Svg11               as S
 import qualified Text.Blaze.Svg11.Attributes    as A
+import           Text.XML.HXT.Core              hiding (when)
+
+locations :: [(Integer, Integer)]
+locations = [ (631, 528) -- Barker
+            , (818, 774) -- 4
+            , (1024, 968) -- Hayden
+            , (742, 280) -- 38
+            , (1151, 530) -- 56
+            , (1343, 551) -- 66
+            , (1956, 1008) -- 51
+            , (424, 652) -- Rotch
+            , (153, 481) -- w20
+            ]
+
+-- | bimap (+)
+mergeOccupancies :: (Integer, Integer)
+                 -> (Integer, Integer)
+                 -> (Integer, Integer)
+mergeOccupancies (a, b) (c, d) = (a+c, b+d)
+
+-- | Partial coercion function
+getInts :: [String] -> (Integer, Integer)
+getInts (a:b:[]) = (read a, read b)
 
 main :: IO ()
 main = do
-  putStrLn . renderSvg $ circleSvg 81 100
+  occupanciesStr <- readProcess "./larvnet.sh" [] []
+  let ocLines = lines occupanciesStr
+      occupanciesLists = map (\l -> [(!! 4), (!! 5)] <*> [words l]) ocLines -- Can error
+      occupanciesPreBarker = map getInts occupanciesLists -- Can error
+      occupancies = map (freCurToCurMax) $ mergeOccupancies (head occupanciesPreBarker) (head . tail $ occupanciesPreBarker) : (tail . tail $ occupanciesPreBarker)
+  putStrLn $ show occupanciesLists
+  putStrLn $ show occupancies
+  void $ runX ( readDocument [] "base.svg"
+                >>>
+                (foldl1' (>>>) (map cAddCircle $ zip occupancies locations))
+                >>>
+                writeDocument [] "cview.svg"
+              )
+  where
+    freCurToCurMax :: (Integer, Integer) -> (Integer, Integer)
+    freCurToCurMax (fre, cur) = (cur, fre+cur)
 
+cAddCircle :: (ArrowXml a, Show s) => ((Integer, Integer), (s, s)) -> a XmlTree XmlTree
+cAddCircle ((a, b), (c, d)) = addCircle a b c d
+
+addCircle :: (ArrowXml a, Show s) => Integer -- Current occupancy
+          -> Integer -- Maximum occupancy
+          -> s -- x coord
+          -> s -- y coord
+          -> a XmlTree XmlTree
+addCircle cur max x y = processTopDownUntil $ (hasName "svg") `guards`
+            (replaceChildren ( getChildren
+                               <+>
+                               ( parseBlaze (circleSvg cur max)
+                                 >>>
+                                 adjustPlacement x y 0.7 0.7
+                               )
+                             )
+            )
+
+-- | A helper function for transform
+adjustPlacement :: (ArrowXml a, Show p, Show s) => p -> p -> s -> s -> a XmlTree XmlTree
+adjustPlacement x y sx sy = transform $ "translate(" ++ show x ++ "," ++ show y ++ ") " ++
+                                        "scale(" ++ show sx ++ "," ++ show sy ++ ")"
+
+-- | An arrow that applies a transformation to the current node
+transform :: (ArrowXml a) => String -> a XmlTree XmlTree
+transform s = addAttrl (sattr "transform" s)
+
+-- | Convert a blaze representation to an arrow which produces an HXT
+-- representation
+parseBlaze :: (ArrowXml a) => S.Svg -> a b XmlTree
+parseBlaze svg = arr (const $ renderSvg svg)
+                 >>>
+                 xread
+
+-- | Circle fragment
 circleSvg :: Integer -- Current occupancy
           -> Integer -- Maximum occupancy
           -> S.Svg
-circleSvg cur max = S.docTypeSvg ! A.version "1.1" ! A.width "100" ! A.height "100" ! A.viewbox "0 0 100 100" $ do
-  -- Green segment
-  when (cur > 0) $ S.path ! A.d (arcPath 0 (fromInteger cur / fromInteger max)) ! A.fill "#00FF00"
-  -- Red segment
-  when (cur < max) $ S.path ! A.d (arcPath (fromInteger cur / fromInteger max) 1) ! A.fill "#FF0000"
+circleSvg cur max = S.g $ do -- no doctype
+  -- (will fail at extremes, because elliptical arcs can't draw circles)
+  when (cur > 0 && cur < max) $ do
+    -- Red segment
+    S.path ! A.d (arcPath 0 (fromInteger cur / fromInteger max)) ! A.fill "#FF0000"
+    -- Green segment
+    S.path ! A.d (arcPath (fromInteger cur / fromInteger max) 1) ! A.fill "#05a727"
+  -- Backup green circle
+  when (cur == 0) $ S.circle ! A.cx "50" ! A.cy "50" ! A.r "50" ! A.fill "#05a727"
+  -- Backup red circle
+  when (cur == max) $ S.circle ! A.cx "50" ! A.cy "50" ! A.r "50" ! A.fill "#FF0000"
   -- White circle
   S.circle ! A.cx "50" ! A.cy "50" ! A.r "40" ! A.fill "#FFFFFF"
   -- Number overlay
-  S.text_ ! A.y "60" ! A.x "20" ! A.fontSize "50" $ fromString $ show cur
+  S.text_ ! A.y "60" !
+            A.x "50" !
+            A.style "text-anchor: middle; font-family: Droid Sans; font-size: 33;" $ fromString $ show (max - cur)
 
 -- | Elliptical Arc
 a :: Show a => a -> a -> a -> a -> a -> a -> a -> S.Path
